8d2ae956400324cd3236c4c635c46e9970fd9677
Justin Horner <justinh.horner@gmail.com>
2021-04-17 11:07:30 -0400

    Add not found page

--- src/pages/module.ts
+++ src/pages/module.ts
@@ -1,5 +1,7 @@
 import { WelcomePage } from './welcome/welcome';
+import { NotFoundPage } from './not-found/not-found';
 
 export const pages = [
-  new WelcomePage()
+  new WelcomePage(),
+  new NotFoundPage()
 ];
