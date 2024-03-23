42df9cfdeddd8c3cd7588bd69d657b01d3850bd8
Joey Bayes <joey.bayes@uscca.com>
2024-03-23 09:26:43 -0400

    Add on build start and end triggers.  Target service-worker as entry point.

--- build.mjs
+++ build.mjs
@@ -3,27 +3,33 @@ import * as esbuild from 'esbuild';
 import * as fs from 'fs';
 
 const args = process.argv.slice(2);
+const isProductionBuild = args[0] === 'production';
 
-if (fs.existsSync('public')) {
-  fs.rmSync('public', { force: true, recursive: true }, (e) => console.error(e));
-}
+const buildStaticFiles = () => {
+  if (fs.existsSync('public')) {
+    fs.rmSync('public', { force: true, recursive: true }, (e) => console.error(e));
+  }
 
-fs.mkdirSync('public', {}, (e) => console.error(e));
-fs.cpSync('src/wwwroot', 'public', { recursive: true });
-// fs.cpSync('src/index.html', 'public/index.html');
-const indexContents = fs.readFileSync('src/index.html').toString()
-  .replace('</head>', `  <link rel="stylesheet" href="index.css?${Date.now()}">
-</head>`)
-  .replace('</body>', `  <script src="index.js?${Date.now()}"></script>
-</body>`)
-  // eslint-disable-next-line max-len
-  .replace('</body>', args[0] === 'production' ? '</body>' : `  <script>new EventSource('/esbuild').addEventListener('change', () => location.reload())</script>
-</body>`);
-fs.writeFileSync('public/index.html', indexContents);
+  fs.mkdirSync('public', {}, (e) => console.error(e));
+  fs.cpSync('src/wwwroot', 'public', { recursive: true });
+  // fs.cpSync('src/index.html', 'public/index.html');
+  const indexContents = fs.readFileSync('src/index.html').toString()
+    .replace('</head>', `  <link rel="stylesheet" href="index.css?${Date.now()}">
+  </head>`)
+    .replace('</body>', `  <script src="index.js?${Date.now()}"></script>
+  </body>`)
+    // eslint-disable-next-line max-len
+    .replace('</body>', isProductionBuild ? '</body>' : `  <script>new EventSource('/esbuild').addEventListener('change', () => location.reload())</script>
+  </body>`);
+  fs.writeFileSync('public/index.html', indexContents);
+};
 
-if (args[0] === 'production') {
+buildStaticFiles();
+const entryPoints = ['src/index.ts', 'src/service-worker.ts'];
+
+if (isProductionBuild) {
   await esbuild.build({
-    entryPoints: ['src/index.ts'],
+    entryPoints,
     bundle: true,
     outdir: 'public',
     minify: true,
@@ -31,9 +37,18 @@ if (args[0] === 'production') {
   });
 } else {
   const context = await esbuild.context({
-    entryPoints: ['src/index.ts'],
+    entryPoints,
     bundle: true,
-    outdir: 'public'
+    outdir: 'public',
+    plugins: [{
+      name: 'onBuild',
+      setup(build) {
+        build.onStart(() => {
+          buildStaticFiles();
+        });
+        build.onEnd(r => console.log(`Build completed: ${JSON.stringify(r)}`));
+      }
+    }]
   });
 
   await context.watch();
